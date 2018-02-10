c9148a932b9e80407077c7c90bb34d57703d42de
Wallrunner93 <wallrunner93@gmail.com>
2018-02-10 13:42:24 -0600

    Commit con los cambios necesarios para la continuación del ejericio

--- app/src/main/java/com/diplo/wallrunner/gitandroid/MainActivity.java
+++ app/src/main/java/com/diplo/wallrunner/gitandroid/MainActivity.java
@@ -2,12 +2,36 @@ package com.diplo.wallrunner.gitandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnPresiona, btnHazlo;
+    private TextView tvMensaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPresiona = findViewById(R.id.button);
+        btnHazlo = findViewById(R.id.button2);
+        tvMensaje = findViewById(R.id.textView);
+
+        btnPresiona.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvMensaje.setText("Hola GitHub!!");
+            }
+        });
+
+        btnHazlo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvMensaje.setText("Hola Diplomado Android!!");
+            }
+        });
     }
 }
